226431fdaf7491270cdd9b00c74e42615f247826
eleaco <eleaco0128@gmail.com>
2020-10-12 10:17:21 +0900

    ホームページとdeviseを用いた新規登録とログインを作成中

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do |i|
+    i.validates :nickname
+    i.validates :email
+    i.validates :password
+    i.validates :encrypted_password
+  end
+
+  PASSWORD_REGEX =  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
