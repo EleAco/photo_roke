d4f081d2d37b92b35ce7c44119793ab9d000b6d0
eleaco <eleaco0128@gmail.com>
2020-10-12 18:54:14 +0900

    各ページのクラス名の重複の改善

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,4 +3,11 @@ class HomesController < ApplicationController
   def index
   end
 
+  def new_guest
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsefe_base64
+    end
+    sing_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
+  end
 end
