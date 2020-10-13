3adc67025f317d80dc2d2f9435c7dfb55d237aa6
eleaco <eleaco0128@gmail.com>
2020-10-13 16:53:53 +0900

    deviseを用いたゲストログインの実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,9 +5,9 @@ class HomesController < ApplicationController
 
   def new_guest
     user = User.find_or_create_by(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsefe_base64
+      user.password = SecureRandom.urlsafe_base64
     end
-    sing_in user
+    sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
   end
 end
