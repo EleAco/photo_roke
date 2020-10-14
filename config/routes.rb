47687590369075e48c04018f7afec0b0b62f240a
eleaco <eleaco0128@gmail.com>
2020-10-14 16:30:03 +0900

    deviseを使用した簡単ログインの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/homes/guest_sign_in', to: 'homes#new_guest'
-  root to: 'homes#index'
+  root to: 'photos#index'
+  get '/homes/home', to:'homes#index'
 end
