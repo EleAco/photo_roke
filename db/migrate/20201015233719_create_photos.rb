140b567efde22ff7afa5716c171a7c1ef69c9496
eleaco <eleaco0128@gmail.com>
2020-10-17 10:02:28 +0900

    カラムを記述

--- db/migrate/20201015233719_create_photos.rb
+++ db/migrate/20201015233719_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :photos do |t|
-
+      t.text         :explanation,         null: false
       t.timestamps
     end
   end
