3c0d15ea6fef16b099f7814116193d06d3684b20
eleaco <eleaco0128@gmail.com>
2020-10-22 11:47:08 +0900

    jQueryの実装

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,13 @@
+class FavoritesController < ApplicationController
+  def create
+    favorite = current_user.favorites.build(photo_id: params[:photo_id])
+    favorite.save
+    redirect_to photos_path
+  end
+
+  def destroy
+    favorite = Favorite.find_by(photo_id: params[:photo_id], user_id: current_user.id)
+    favorite.destroy
+    redirect_to photos_path
+  end
+end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,11 @@ class Photo < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments
+  has_many :favorites
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
   def self.search(search)
     if search != ""
