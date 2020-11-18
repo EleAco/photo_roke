81aac7fec19b127ed2ef1caf10349789c85d645c
eleaco <eleaco0128@gmail.com>
2020-11-18 15:26:04 +0900

    緯度経度説明文値がなくても登録できるように記述

--- db/migrate/20201015233719_create_photos.rb
+++ db/migrate/20201015233719_create_photos.rb
@@ -1,11 +1,11 @@
 class CreatePhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :photos do |t|
-      t.text         :explanation,         null: false
+      t.text         :explanation
       t.string       :title,               null: false
       t.integer      :user_id,             null: false, foreign_key: true
-      t.float        :latitude,            null: false
-      t.float        :longitude,           null: false
+      t.float        :latitude
+      t.float        :longitude
       t.timestamps   
     end
   end
