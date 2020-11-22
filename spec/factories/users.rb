82338ae12773113aee7f511926efdb815d77c3c2
eleaco <eleaco0128@gmail.com>
2020-11-22 20:27:59 +0900

    Fakerを導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname                  { Faker::Name.intials(number: 7) }
+    nickname                  { Faker::Name.initials(number: 6) }
     email                     { Faker::Internet.free_email }
     password                  { Faker::Internet.password(min_length: 6)}
     password_confirmation     { password } 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+   @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規機能' do
+    context '新規登録がうまく行く時' do
+      it 'nickname,email,password,password_confirmationが存在すれば登録できる' do
+        ecpect(@user).to be_valid
+      end
+    end
+  end
 end
