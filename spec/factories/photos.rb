b9b3890091685030a158407de06bbc3c02ea3c3f
eleaco <eleaco0128@gmail.com>
2020-11-23 09:31:39 +0900

    photoモデルのテストコードを記述

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :photo do
-    
+    title               {'20字ギリギリを記述してテストを行いま'}
+    association :user
   end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @photo = FactoryBot.build(:photo)
+    @photo.image = fixture_file_upload('app/assets/images/photo.jpg')
+  end
+
+  describe '新規投稿' do
+    context '投稿がうまく行く時' do
+      it 'title,imageが存在すれば登録できる' do
+        expect(@photo).to be_valid
+      end
+
+      it 'titleが20字以内ならば、登録ができる' do
+        expect(@photo).to be_valid
+      end
+    end
+  
+    context '投稿がうまくいかない時' do
+      it 'imgaeが空だと登録できない' do
+        @photo.image = nil
+        @photo.valid?
+        expect(@photo.errors.full_messages).to include("写真 は必須です")
+      end
+
+      it 'titleが20字以上だと登録できない' do
+        @photo.title = 'titleが20字以上だと登録できないことをテストします'
+        @photo.valid?
+        expect(@photo.errors.full_messages).to include('作品のタイトル名 は20字以内で登録してください。')
+      end
+    end
+  end
 end
+
