4031922989cab4c3df63ebdda52daa83150606a1
eleaco <eleaco0128@gmail.com>
2020-11-23 15:57:51 +0900

    explanationのテストを記述

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :photo do
-    title               {'20字ギリギリを記述してテストを行いま'}
+    title               {Faker::Lorem.characters(number:20)}
+    explanation         { Faker::Lorem.characters(number:500)}
     association :user
   end
 end
